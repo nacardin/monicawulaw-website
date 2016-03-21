604faff125a57db109babb2df482dec591878475
Nicholas Alan Cardin <nacardin@gmail.com>
2016-03-20 21:19:05 -0400

    deploy script

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,3 @@
-// @AngularClass
-
 var webpack = require('webpack');
 var helpers = require('./helpers');
 
@@ -7,29 +5,21 @@ var CopyWebpackPlugin = require('copy-webpack-plugin');
 var HtmlWebpackPlugin = require('html-webpack-plugin');
 var ForkCheckerPlugin = require('awesome-typescript-loader').ForkCheckerPlugin;
 
-var ENV = process.env.ENV = process.env.NODE_ENV = 'development';
+var ENV = process.env.ENV || 'development';
 var HMR = helpers.hasProcessFlag('hot');
 
 var metadata = {
-  baseUrl: '/',
+  baseUrl: process.env.BASE_URL || '/',
   host: 'localhost',
   port: 3000,
   ENV: ENV,
   HMR: HMR
 };
-/*
- * Config
- * with default values at wgit statebpack.default.conf
- */
+
 module.exports = {
-  // static data for index.html
   metadata: metadata,
   devtool: 'cheap-module-eval-source-map',
-  // cache: true,
   debug: true,
-  // devtool: 'eval' // for faster builds use 'eval'
-
-  // our angular app
   entry: {
     'polyfills': './src/polyfills.ts',
     'vendor': './src/vendor.ts',
@@ -40,31 +30,21 @@ module.exports = {
     extensions: ['', '.ts', '.js']
   },
 
-  // Config for our build files
   output: {
     path: helpers.root('dist'),
-    filename: '[name].bundle.js',
-    sourceMapFilename: '[name].map',
-    chunkFilename: '[id].chunk.js'
+    filename: '[name].[chunkhash].bundle.js',
+    sourceMapFilename: '[name].[chunkhash].map',
+    chunkFilename: '[id].[chunkhash].chunk.js'
   },
 
   module: {
     preLoaders: [
-      // { test: /\.ts$/, loader: 'tslint-loader', exclude: [ helpers.root('node_modules') ] },
-      // TODO(gdi2290): `exclude: [ helpers.root('node_modules/rxjs') ]` fixed with rxjs 5 beta.3 release
       { test: /\.js$/, loader: "source-map-loader", exclude: [ helpers.root('node_modules/rxjs') ] }
     ],
     loaders: [
-      // Support for .ts files.
       { test: /\.ts$/, loader: 'awesome-typescript-loader', exclude: [ /\.(spec|e2e)\.ts$/ ] },
-
-      // Support for *.json files.
       { test: /\.json$/,  loader: 'json-loader' },
-
-      // Support for CSS as raw text
       { test: /\.css$/,   loader: 'raw-loader' },
-
-      // support for .html as raw text
       { test: /\.html$/,  loader: 'raw-loader', exclude: [ helpers.root('src/index.html') ] },
       {
         test: /\.scss$/,
@@ -78,11 +58,8 @@ module.exports = {
     new ForkCheckerPlugin(),
     new webpack.optimize.OccurenceOrderPlugin(true),
     new webpack.optimize.CommonsChunkPlugin({ name: ['app', 'vendor', 'polyfills'], minChunks: Infinity }),
-    // static assets
     new CopyWebpackPlugin([ { from: 'src/assets', to: 'assets' } ]),
-    // generating html
     new HtmlWebpackPlugin({ template: 'src/index.html', chunksSortMode: 'none' }),
-    // Environment helpers (when adding more properties make sure you include them in custom-typings.d.ts)
     new webpack.DefinePlugin({
       'ENV': JSON.stringify(metadata.ENV),
       'HMR': HMR
