947eb0043cd11289535e2f639766df8f28950ec1
Nick Cardin <nick@cardin.email>
2016-11-20 22:58:24 -0500

    finish removing angular 2

--- gulpfile.js
+++ gulpfile.js
@@ -1,17 +1,37 @@
 const gulp = require('gulp');
-const nunjucks = require('gulp-nunjucks');
+const plugins = require('gulp-load-plugins')();
+const autoprefixer = require('autoprefixer');
 const del = require('del');
 const mkdirp = require('mkdirp');
-var browserSync = require('browser-sync').create();
+const browserSync = require('browser-sync').create();
 
 
-gulp.task('build', function() {
+gulp.task('clean-dist', function() {
   del.sync('dist');
   mkdirp.sync('dist');
-  gulp.src('src/*.html')
-    .pipe(nunjucks.compile({ year: (new Date).getFullYear() }))
+});
+
+gulp.task('build-assets', ['clean-dist'], function() {
+  gulp.src('src/assets/**')
+    .pipe(gulp.dest('dist/assets'));
+});
+
+gulp.task('build-styles', ['clean-dist', 'build-assets'], function() {
+  gulp.src('./src/app.scss')
+    .pipe(plugins.sourcemaps.init())
+    .pipe(plugins.sass().on('error', plugins.sass.logError))
+    .pipe(plugins.postcss([ autoprefixer({ browsers: ['last 2 versions'] }) ]))
+    .pipe(plugins.sourcemaps.write('.'))
     .pipe(gulp.dest('dist'));
 });
+
+gulp.task('build-html', ['clean-dist'], function() {
+  gulp.src('src/*.html')
+    .pipe(plugins.nunjucks.compile({ year: (new Date).getFullYear() }))
+    .pipe(gulp.dest('dist'));
+});
+
+gulp.task('build', ['build-html', 'build-assets', 'build-styles']);
 
 gulp.task('watch', ['build'], function() {
   gulp.watch('./src/**', ['build']);
