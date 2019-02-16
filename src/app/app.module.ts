f3aab9e1efedb72784bce7ab33ce87a4bc1e2bf2
Nick Cardin <nick@cardin.email>
2019-02-15 21:35:12 -0500

    Initial commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,30 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { registerLocaleData } from '@angular/common';
+import localeZh from '@angular/common/locales/zh';
+
+import { TransferHttpCacheModule } from '@nguniversal/common';
+
+import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+
+
+registerLocaleData(localeZh, 'zh');
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomeComponent,
+  ],
+  imports: [
+    BrowserModule.withServerTransition({ appId: 'monicawulaw-website' }),
+    RouterModule.forRoot([
+      { path: '', component: HomeComponent, pathMatch: 'full' }
+    ]),
+    TransferHttpCacheModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
